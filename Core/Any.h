1acfaa6b235c40916756cfcd13893570b5891d39
sqz <szy73711@126.com>
2019-10-11 18:07:05 +0800

    Keep working on Any

--- Core/Any.h
+++ Core/Any.h
@@ -6,19 +6,113 @@ CT_SCOPE_BEGIN
 
 namespace AnyInternal
 {
-    class DynamicDataBase
+class IDynamicData
+{
+public:
+    virtual ~IDynamicData() = default;
+    virtual IDynamicData *Clone() const = 0;
+};
+
+template <typename T>
+class DynamicData : public IDynamicData
+{
+public:
+    T value;
+
+    DynamicData(const T &val) : value(val)
     {
+    }
 
-    };
+    DynamicData(T &&val) : value(std::move(val))
+    {
+    }
 
-}
+    IDynamicData *Clone() const override
+    {
+        return ::new DynamicData(value);
+    }
+};
+} // namespace AnyInternal
 
 class Any
 {
+public:
+    Any() = default;
 
+    Any(const Any &other)
+    {
+        if (other.data)
+            data = other.data->Clone();
+    }
 
+    Any(Any &&other) : data(other.data)
+    {
+        other.data = nullptr;
+    }
 
+    Any &operator=(const Any &other)
+    {
+        if (this != &other)
+        {
+            Any temp(other);
+            Swap(temp);
+        }
+        return *this;
+    }
+
+    Any &operator=(Any &&other)
+    {
+        if (this != &other)
+        {
+            Any temp(std::move(other));
+            Swap(temp);
+        }
+        return *this;
+    }
+
+    template <typename T>
+    Any(T &&value) : data(new AnyInternal::DynamicData<T>(std::forward<T>(value)))
+    {
+    }
+
+    template <typename T>
+    Any &operator=(T &&value)
+    {
+        Any temp(std::forward<T>(value));
+        Swap(temp);
+        return *this;
+    }
+
+    ~Any()
+    {
+        Clear();
+    }
+
+    bool IsEmpty() const
+    {
+        return data == nullptr;
+    }
+
+    void Swap(Any &other)
+    {
+        std::swap(data, other.data);
+    }
+
+    void Clear()
+    {
+        if (data)
+            delete data;
+        data = nullptr;
+    }
+
+    template <typename T>
+    T GetValue() const
+    {
+        return static_cast<AnyInternal::DynamicData<T> *>(data)->value;
+    }
+
+private:
+    AnyInternal::IDynamicData *data = nullptr;
 };
-
 
 CT_SCOPE_END
