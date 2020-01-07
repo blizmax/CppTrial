0dbbcc66e2736bf203962c54311472e52bfbe499
sqz <szy73711@126.com>
2020-01-07 16:48:17 +0800

    Keep working on AVLTree

--- Core/Container/AVLTree.h
+++ Core/Container/AVLTree.h
@@ -9,11 +9,11 @@ template <typename Element>
 class Node
 {
 public:
-    Element element;
     Node *left = nullptr;
     Node *right = nullptr;
     int32 leftHeight = 0;
     int32 rightHeight = 0;
+    Element element;
 
     Node(const Element &element) : element(element)
     {
@@ -37,7 +37,205 @@ public:
     using Key = typename KeyTraits::KeyType;
     using NodeType = AVLTreeInternal::Node<Element>;
 
+public:
+    AVLTree() = default;
+
+    AVLTree(const AVLTree &other) : size(other.size)
+    {
+        root = CopyFrom(other.root);
+    }
+
+    AVLTree(AVLTree &&other) noexcept : size(other.size), root(other.root)
+    {
+        other.size = 0;
+        other.root = nullptr;
+    }
+
+    AVLTree &operator=(const AVLTree &other)
+    {
+        if (this != &other)
+        {
+            AVLTree temp(other);
+            Swap(temp);
+        }
+        return *this;
+    }
+
+    AVLTree &operator=(AVLTree &&other) noexcept
+    {
+        if (this != &other)
+        {
+            ClearPrivate(root);
+
+            size = other.size;
+            root = other.root;
+            other.size = 0;
+            other.root = nullptr;
+        }
+        return *this;
+    }
+
+    ~AVLTree()
+    {
+        ClearPrivate(root);
+    }
+
+    SizeType Size() const
+    {
+        return size;
+    }
+
+    bool IsEmpty() const
+    {
+        return size == 0;
+    }
+
+    void Swap(AVLTree &other)
+    {
+        if (this != &other)
+        {
+            std::swap(size, other.size);
+            std::swap(root, other.root);
+        }
+    }
+
+    void Clear()
+    {
+        ClearPrivate(root);
+        root = nullptr;
+        size = 0;
+    }
+
+    NodeType *Find(const Element &value) const
+    {
+        const Key &key = KeyTraits::GetKey(value);
+        return FindPrivate(key, root);
+    }
+
+    NodeType *FindByKey(const Key &key) const
+    {
+        return FindPrivate(key, root);
+    }
+
+    bool Contains(const Element &value) const
+    {
+        return Find(value) != nullptr;
+    }
+
+    bool ContainsKey(const Key &key) const
+    {
+        return FindByKey(key) != nullptr;
+    }
+
+    void Put(const Element &value)
+    {
+        const Key &key = KeyTraits::GetKey(value);
+
+        root = InsertPrivate(key, value, root, nullptr);
+    }
+
+    void Put(Element &&value)
+    {
+        const Key &key = KeyTraits::GetKey(value);
+
+        root = InsertPrivate(key, std::move(value), root, nullptr);
+    }
+
+    bool Add(const Element &value)
+    {
+        const Key &key = KeyTraits::GetKey(value);
+
+        bool existed = false;
+        root = InsertPrivate(key, value, root, &existed);
+        return !existed;
+    }
+
+    bool Add(Element &&value)
+    {
+        const Key &key = KeyTraits::GetKey(value);
+
+        bool existed = false;
+        root = InsertPrivate(key, std::move(value), root, &existed);
+        return !existed;
+    }
+
+    Element &Get(const Element &value)
+    {
+        NodeType *node = Find(value);
+        CheckRange(node);
+        return node->element;
+    }
+
+    const Element &Get(const Element &value) const
+    {
+        NodeType *node = Find(value);
+        CheckRange(node);
+        return node->element;
+    }
+
+    Element &GetByKey(const Key &key)
+    {
+        NodeType *node = FindByKey(key);
+        CheckRange(node);
+        return node->element;
+    }
+
+    const Element &GetByKey(const Key &key) const
+    {
+        NodeType *node = FindByKey(key);
+        CheckRange(node);
+        return node->element;
+    }
+
+    bool Remove(const Element &value)
+    {
+        return RemoveByKey(KeyTraits::GetKey(value));
+    }
+
+    bool RemoveByKey(const Key &key)
+    {
+        bool existed = true;
+        root = RemovePrivate(key, root, &existed);
+        return existed;
+    }
+
+    bool operator==(const AVLTree &other) const
+    {
+        if (size != other.size)
+        {
+            return false;
+        }
+
+        //TODO
+
+        return true;
+    }
+
+    bool operator!=(const AVLTree &other) const
+    {
+        return !(*this == other);
+    }
+
+    //===================== STL STYLE =========================
+public:
+    friend class Iterator;
+    friend class ConstIterator;
+
+    //TODO
+    class Iterator
+    {
+    };
+
+    class ConstIterator
+    {
+    };
+
 private:
+    void CheckRange(NodeType *node) const
+    {
+        CT_ASSERT(node != nullptr);
+    }
+
     int32 Compare(const Key &key1, const Key &key2) const
     {
         static Comparer comp;
@@ -86,10 +284,56 @@ private:
         }
     }
 
-    NodeType *InsertPrivate(const Key &key, const Element &value, NodeType *node, bool *updated)
+    NodeType *FindMinPrivate(NodeType *node) const
+    {
+        if (node->left)
+        {
+            return FindMinPrivate(node->left);
+        }
+        return node;
+    }
+
+    NodeType *FindMaxPrivate(NodeType *node) const
+    {
+        if (node->right)
+        {
+            return FindMaxPrivate(node->right);
+        }
+        return node;
+    }
+
+    NodeType *CopyFrom(NodeType *node)
     {
         if (!node)
         {
+            return nullptr;
+        }
+
+        NodeType *newNode = CreateNode(node->element);
+        newNode->left = CopyFrom(node->left);
+        newNode->right = CopyFrom(node->right);
+        newNode->leftHeight = node->leftHeight;
+        newNode->rightHeight = node->rightHeight;
+        return newNode;
+    }
+
+    void ClearPrivate(NodeType *node)
+    {
+        if (!node)
+        {
+            return;
+        }
+
+        ClearPrivate(node->left);
+        ClearPrivate(node->right);
+        DeleteNode(node);
+    }
+
+    NodeType *InsertPrivate(const Key &key, const Element &value, NodeType *node, bool *existed)
+    {
+        if (!node)
+        {
+            ++size;
             return CreateNode(value);
         }
 
@@ -97,21 +341,118 @@ private:
         auto val = Compare(KeyTraits::GetKey(node->element), key);
         if (val == 0)
         {
-            if (updated != nullptr)
+            if (existed == nullptr)
             {
                 node->element = value;
-                *updated = true;
+            }
+            else
+            {
+                *existed = true;
             }
         }
         else if (val < 0)
         {
-            node->right = InsertPrivate(key, value, node->right, updated);
+            node->right = InsertPrivate(key, value, node->right, existed);
             node->rightHeight = node->right->Height();
         }
         else
         {
-            node->left = InsertPrivate(key, value, node->left, updated);
+            node->left = InsertPrivate(key, value, node->left, existed);
             node->leftHeight = node->left->Height();
+        }
+
+        return Balance(ret);
+    }
+
+    NodeType *InsertPrivate(const Key &key, Element &&value, NodeType *node, bool *existed)
+    {
+        if (!node)
+        {
+            ++size;
+            return CreateNode(std::move(value));
+        }
+
+        NodeType *ret = node;
+        auto val = Compare(KeyTraits::GetKey(node->element), key);
+        if (val == 0)
+        {
+            if (existed == nullptr)
+            {
+                node->element = std::move(value);
+            }
+            else
+            {
+                *existed = true;
+            }
+        }
+        else if (val < 0)
+        {
+            node->right = InsertPrivate(key, std::move(value), node->right, existed);
+            node->rightHeight = node->right->Height();
+        }
+        else
+        {
+            node->left = InsertPrivate(key, std::move(value), node->left, existed);
+            node->leftHeight = node->left->Height();
+        }
+
+        return Balance(ret);
+    }
+
+    NodeType *RemovePrivate(const Key &key, NodeType *node, bool *existed)
+    {
+        if (!node)
+        {
+            *existed = false;
+            return nullptr;
+        }
+
+        NodeType *ret = node;
+        auto val = Compare(KeyTraits::GetKey(node->element), key);
+        if (val == 0)
+        {
+            if (!node->left && !node->right)
+            {
+                DeleteNode(node);
+                ret = nullptr;
+                --size;
+            }
+            else if (node->left && node->right)
+            {
+                NodeType *max = FindMaxPrivate(node->left);
+                NodeType *newNode = CreateNode(max->element);
+                newNode->left = RemovePrivate(KeyTraits::GetKey(newNode->element), node->left, existed);
+                newNode->right = node->right;
+                newNode->leftHeight = newNode->left ? newNode->left->Height() : 0;
+                newNode->rightHeight = node->rightHeight;
+
+                DeleteNode(node);
+                ret = newNode;
+            }
+            else
+            {
+                NodeType *temp = (node->left ? node->left : node->right);
+
+                DeleteNode(node);
+                ret = temp;
+                --size;
+            }
+        }
+        else if (val < 0)
+        {
+            node->right = RemovePrivate(key, node->right, existed);
+            if (*existed)
+            {
+                node->rightHeight = node->right ? node->right->Height() : 0;
+            }
+        }
+        else
+        {
+            node->left = RemovePrivate(key, node->left, existed);
+            if (*existed)
+            {
+                node->leftHeight = node->left ? node->left->Height() : 0;
+            }
         }
 
         return Balance(ret);
