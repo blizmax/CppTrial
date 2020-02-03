c75024f70a76fb6f320bc1966329208fe550a436
Abenmonkey <szy73711@126.com>
2020-02-04 03:03:09 +0800

    Keep working on Json lib

--- Json/JsonWriter.cpp
+++ Json/JsonWriter.cpp
@@ -1,33 +1,70 @@
 #include "Json/JsonWriter.h"
 
-Json::JsonWriter::JsonWriter()
-{
-    stack.Push(JsonType::Object);
-}
-
 Json::JsonWriter& Json::JsonWriter::Name(const String& name)
 {
     if(CheckCanWriteName())
     {
         named = true;
-    }
 
+        if (GetCurrentState().first)
+            GetCurrentState().first = false;
+        else
+            buffer += CT_TEXT(", ");
+
+        buffer += CT_TEXT('\"');
+        buffer += name;
+        buffer += CT_TEXT('\"');
+        buffer += CT_TEXT(": ");
+    }
 
     return *this;
 }
 
-Json::JsonWriter& Json::JsonWriter::PushObject()
+Json::JsonWriter &Json::JsonWriter::PushObject()
 {
-
+    if (CheckCanPushState())
+    {
+        stack.Push({false, true});
+        buffer += CT_TEXT('{');
+    }
+    return *this;
 }
 
-bool Json::JsonWriter::CheckCanWriteName() const
+Json::JsonWriter::State &Json::JsonWriter::GetCurrentState()
 {
-    if(named)
+    if(stack.IsEmpty())
     {
-        return false;
+        PushObject();
     }
-    if(stack.Top() != JsonType::Object)
+    return stack.Top();
+}
+
+bool Json::JsonWriter::CheckCanPushState()
+{
+    if (stack.IsEmpty())
+        return true;
+
+    State &state = stack.Top();    
+    if(state.array)
+    {
+        if(state.first)
+            state.first = false;
+        else
+            buffer += CT_TEXT(", ");
+    }
+    else
+    {
+        if(!named)
+            return false;
+        else
+            named = false; 
+    }
+    return true;
+}
+
+bool Json::JsonWriter::CheckCanWriteName()
+{
+    if (named || GetCurrentState().array)
     {
         return false;
     }
--- Json/JsonWriter.h
+++ Json/JsonWriter.h
@@ -9,21 +9,37 @@ namespace Json
 class JsonWriter
 {
 public:
-    JsonWriter();
-    ~JsonWriter() = default;
+    JsonWriter &PushObject();
+    JsonWriter &PushArray();
+    JsonWriter &Pop();
+    JsonWriter &Name(const String &name);
 
-    JsonWriter& PushObject();
-    JsonWriter& PushArray();
-    JsonWriter& Name(const String& name);
-    
-    template<typename T>
-    void Value(T&&);
+    JsonWriter &Value(const String &value);
+
+    JsonWriter &Value(std::nullptr_t)
+    {
+        return Value(String(CT_TEXT("null")));
+    }
+
+    template <typename T, typename = typename TEnableIf<TIsIntegral<T>::value || TIsFloatingPoint<T>::value, T>::value>
+    JsonWriter &Value(T value)
+    {
+        return Value(StringConvert::ToString(value));
+    }
 
 private:
-    bool CheckCanWriteName() const;
+    struct State
+    {
+        bool array;
+        bool first;
+    };
 
-    int32 indentLevel = 0;
+    State &GetCurrentState();
+    bool CheckCanPushState();
+    bool CheckCanWriteName();
+
     bool named = false;
-    Stack<JsonType> stack;
-};     
-}
+    Stack<State> stack;
+    String buffer;
+};
+} // namespace Json
